0910d0683fc1a69464eed8c1572c7cf965a426fc
Kim <xodnjs0876@gmail.com>
2022-03-15 18:52:37 +0900

    List View

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 김태원 on 2022/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
