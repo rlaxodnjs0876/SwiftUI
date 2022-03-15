a9ba59da2196e088d5cf8519d032519838c8f5e3
Kim <xodnjs0876@gmail.com>
2022-03-15 18:51:07 +0900

    App View

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김태원 on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
