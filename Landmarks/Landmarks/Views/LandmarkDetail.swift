6ed82784c49e02045c6fb1d1b5fd64567b0710a3
Kim <xodnjs0876@gmail.com>
2022-03-15 18:54:41 +0900

    Add Details

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,55 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 김태원 on 2022/03/15.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        // 사용자가 스크롤 가능하도록 ScrollView로 변경
+        // MapView, CircleImage, Text  데이터 넘겨주기
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .foregroundColor(.primary)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
