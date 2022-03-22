7c8e99ee35849821266b1769f882cf249489372c
Kim <xodnjs0876@gmail.com>
2022-03-22 22:58:40 +0900

    AppIcon

---
+++ Landmarks/Landmarks/Views/Landmarks/BadgeBackground.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by 김태원 on 2022/03/21.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
---
+++ Landmarks/Landmarks/Views/Landmarks/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by 김태원 on 2022/03/21.
+//
+
+import Foundation
