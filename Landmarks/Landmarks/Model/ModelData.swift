1ad04095212edfa04a9310b325a419d5c84e025d
Kim <xodnjs0876@gmail.com>
2022-03-23 20:10:00 +0900

    Json Load

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -8,18 +8,16 @@
 import Foundation
 import Combine
 
-final class ModelData:ObservableObject{
+final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
 }
-
-
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
-        fatalError("Couldn't find \(filename) in main bundle")
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
     
     do {
