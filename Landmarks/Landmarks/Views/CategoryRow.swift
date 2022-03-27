8a06e72f37a5633944f1a4dc8f4bae025a2f9147
Kim <xodnjs0876@gmail.com>
2022-03-27 22:17:22 +0900

    Category Row

---
+++ Landmarks/Landmarks/Views/CategoryRow.swift
@@ -0,0 +1,45 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by 김태원 on 2022/03/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack(alignment: .top, spacing: 0){
+                    ForEach(items) { landmark in
+                        NavigationLink{
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmakrs = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmakrs[0].category.rawValue, items: Array(landmakrs.prefix(4))
+        )
+    }
+}
