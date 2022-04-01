5b51cd6ac37a0e9169b2ef71d5896a8c94382ba8
Kim <xodnjs0876@gmail.com>
2022-04-01 23:55:38 +0900

    [View] Notification

--- Landmarks/WatchLandmarks WatchKit Extension/NotificationView.swift
+++ Landmarks/WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -11,20 +11,20 @@ struct NotificationView: View {
     var title: String?
     var message: String?
     var landmark: Landmark?
-    
+
     var body: some View {
-        VStack{
+        VStack {
             if landmark != nil {
                 CircleImage(image: landmark!.image.resizable())
                     .scaledToFit()
             }
-            
+
             Text(title ?? "Unknown Landmark")
                 .font(.headline)
-            
+
             Divider()
-            
-            Text(message ?? "You are within 5 miles of one of your favorite landmarks")
+
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
                 .font(.caption)
         }
         .lineLimit(0)
@@ -33,11 +33,12 @@ struct NotificationView: View {
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        Group{
+        Group {
             NotificationView()
             NotificationView(title: "Turtle Rock",
                              message: "You are within 5 miles of Turtle Rock.",
                              landmark: ModelData().landmarks[0])
         }
+
     }
 }
