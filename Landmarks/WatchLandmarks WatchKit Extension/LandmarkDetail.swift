957d494fcdba9115496929e62fd777a8b2e34ad7
Kim <xodnjs0876@gmail.com>
2022-04-01 23:56:37 +0900

    [ADD] WatchOS Detail

--- Landmarks/WatchLandmarks WatchKit Extension/LandmarkDetail.swift
+++ Landmarks/WatchLandmarks WatchKit Extension/LandmarkDetail.swift
@@ -11,20 +11,21 @@ struct LandmarkDetail: View {
     @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
     
-    var landmarkIndex: Int{
-        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
     
     var body: some View {
-        ScrollView{
-            VStack{
+        ScrollView {
+            VStack {
                 CircleImage(image: landmark.image.resizable())
                     .scaledToFit()
                 
                 Text(landmark.name)
                     .font(.headline)
                     .lineLimit(0)
-                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite){
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
                     Text("Favorite")
                 }
                 
@@ -42,7 +43,6 @@ struct LandmarkDetail: View {
                 
                 MapView(coordinate: landmark.locationCoordinate)
                     .scaledToFit()
-                
             }
             .padding(16)
         }
