7884fe9e0c4a7ed347fac2364cc9bcccb6331b58
Kim <xodnjs0876@gmail.com>
2022-06-15 19:54:19 +0900

    [EDIT] MeetingView

--- Scrumdinger/Scrumdinger/Views/Meeting/MeetingView.swift
+++ Scrumdinger/Scrumdinger/Views/Meeting/MeetingView.swift
@@ -20,8 +20,7 @@ struct MeetingView: View {
                 .fill(scrum.theme.mainColor)
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-                Circle()
-                    .strokeBorder(lineWidth: 24, antialiased: true)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
@@ -37,6 +36,8 @@ struct MeetingView: View {
         }
         .onDisappear {
             scrumTimer.stopScrum()
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
+            scrum.history.insert(newHistory, at: 0)
         }
         
         .navigationBarTitleDisplayMode(.inline)
